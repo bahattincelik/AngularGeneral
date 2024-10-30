4a54c0c1b0f4d4c3230c649f29127d8330ac7e60
Bahattin Celik <celik@wave-solutions.de>
2024-10-30 10:28:44 +0100

    ngTemplate

--- AngularLernen12/src/app/app.component.ts
+++ AngularLernen12/src/app/app.component.ts
@@ -1,16 +1,39 @@
-import { NgFor, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
+import {
+  Component,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NgFor, NgIf],
+  imports: [RouterOutlet, HomeComponent, NgFor, NgIf, NgTemplateOutlet],
   template: `
     <h3>App Component</h3>
 
-    <ng-container>Container icerigi</ng-container>
+    <ng-container
+      [ngTemplateOutlet]="t"
+      [ngTemplateOutletContext]="{ data: 123 }"
+      ]
+      >Container <br
+    /></ng-container>
+    <ng-template #t let-data="data"
+      >ngTemplateOutletContext <br />
+      {{ data }}</ng-template
+    >
+    <br />
+    <hr />
+    <ng-container *ngTemplateOutlet="templateIcerik"
+      >Container icerigi</ng-container
+    >
+
+    <ng-template #templateIcerik>Template icerigi</ng-template>
+
+    <ng-template #templateIcerik2>Template icerigi diger Yontem</ng-template>
 
     <ul>
       <span *ngFor="let product of products">
@@ -53,4 +76,12 @@ export class AppComponent {
     { produtcName: 'Eraser', available: true },
     { produtcName: 'Ruler', available: false },
   ];
+
+  @ViewChild('templateIcerik2', { read: TemplateRef })
+  ngTemplate: TemplateRef<any>;
+  constructor(private viewContainerRef: ViewContainerRef) {}
+  ngAfterViewInit() {
+    // Bu kısım, HTML tamamen yüklendiğinde `templateIcerik`'i dolduracak
+    this.viewContainerRef.createEmbeddedView(this.ngTemplate);
+  }
 }
