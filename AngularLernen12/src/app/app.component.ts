c8ef4ebb9a351046cb04e9f0b627e620d0d82e1d
Bahattin Celik <celik@wave-solutions.de>
2024-10-30 09:49:29 +0100

    ngContainer

--- AngularLernen12/src/app/app.component.ts
+++ AngularLernen12/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
@@ -5,9 +6,28 @@ import { HomeComponent } from './components/home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, NgFor, NgIf],
   template: `
     <h3>App Component</h3>
+
+    <ng-container>Container icerigi</ng-container>
+
+    <ul>
+      <span *ngFor="let product of products">
+        <li *ngIf="product.available">
+          {{ product.produtcName }}
+        </li>
+      </span>
+    </ul>
+
+    <ul>
+      <ng-container *ngFor="let product of products">
+        <li *ngIf="product.available">
+          {{ product.produtcName }}
+        </li>
+      </ng-container>
+    </ul>
+
     <app-home
       ><!--Burasi ng-content-->
       <header>Header</header>
@@ -24,4 +44,13 @@ import { HomeComponent } from './components/home/home.component';
 })
 export class AppComponent {
   title = 'AngularLernen12';
+
+  products: any[] = [
+    { produtcName: 'Pencil', available: true },
+    { produtcName: 'Notebook', available: false },
+    { produtcName: 'Pen', available: true },
+    { produtcName: 'Marker', available: false },
+    { produtcName: 'Eraser', available: true },
+    { produtcName: 'Ruler', available: false },
+  ];
 }
