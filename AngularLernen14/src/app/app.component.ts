8c48cf7f8b0f4d821cc635490fb9ec73f2588b4b
Bahattin Celik <celik@wave-solutions.de>
2024-11-08 11:14:20 +0100

    Environments

--- AngularLernen14/src/app/app.component.ts
+++ AngularLernen14/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment.development';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    console.log(environment.production);
+    console.log(environment.apiUrl);
+  }
+}
