2ed00b528a6df56e6781bc60f1a01a027356979e
Bahattin Celik <celik@wave-solutions.de>
2024-11-08 11:04:21 +0100

    Runtime Configuration

--- AngularLernen14/src/main.ts
+++ AngularLernen14/src/main.ts
@@ -1,16 +1,29 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { APP_INITIALIZER } from '@angular/core';
+import { APP_INITIALIZER, importProvidersFrom } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    importProvidersFrom(HttpClientModule),
     {
       provide: APP_INITIALIZER,
-      useFactory: () => () => {
+      /*useFactory: () => () => {
         console.log('App initialization complete');
         return null;
       },
+      multi: true,*/
+
+      useFactory: (httpClient: HttpClient) => () =>
+        httpClient
+          .get('./assets/config/appConfig.json')
+          .toPromise()
+          .then((config) => {
+            console.log(config);
+          }),
+
+      deps: [HttpClient],
       multi: true,
     },
     ...appConfig.providers,
